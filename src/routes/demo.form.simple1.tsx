76aaf58560a8415ebb077d4407a398e82bee0d5a
ShahirTurkmen <shahirturkmen@gmail.com>
2025-04-17 15:21:01 +0430

    Add initial project files, including assets, configuration, and example components

---
+++ src/routes/demo.form.simple1.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/demo/form/simple1')({
+  component: SimpleForm,
+})
+
+function SimpleForm() {
+
+  return (
+    <h1>esrsejfuhseuh</h1>
+  )
+}
